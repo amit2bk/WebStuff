798acdbc6eea89ead1a49e005df584eaecea92ce
amit2bk <amit2bk@yahoo.com>
2016-02-29 21:08:16 -0800

    Initial Grader Code

---
+++ Grader/Grader/GradeStatistics.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grader
+{
+    class GradeStatistics
+    {
+        public float HigestGrade;
+        public float LowestGrade;
+        public float AverageGrade;
+    }
+}
---
+++ Grader/Grader/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace Grader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<float> grades = new List<float>();
+            string path = args[0];
+
+            List<string> lines = new List<string>();
+
+            lines = File.ReadAllLines(path).ToList();
+
+            foreach(string l in lines)
+            {
+                float grade = float.Parse(l);
+
+                grades.Add(grade);
+
+            }
+
+            Program PROG = new Program();
+
+            if (1 == int.Parse(args[1]))
+            {
+
+                Stopwatch watch = new Stopwatch();
+
+                watch.Start();
+
+                float high = PROG.HighestGrade(grades);
+                float low = PROG.LowestGrade(grades);
+                float avg = PROG.AverageGrade(grades);
+
+                Console.WriteLine(high);
+                Console.WriteLine(low);
+                Console.WriteLine(avg);
+
+                watch.Stop();
+
+                Console.WriteLine("Time taken to execute is : " + watch.ElapsedTicks);
+            }
+            else
+            {
+                Stopwatch watch = new Stopwatch();
+
+                watch.Start();
+
+                GradeStatistics stats = new GradeStatistics();
+
+                stats = PROG.ComputeStatistics(grades);
+
+                Console.WriteLine(stats.AverageGrade);
+                Console.WriteLine(stats.LowestGrade);
+                Console.WriteLine(stats.HigestGrade);
+
+                watch.Stop();
+
+                Console.WriteLine("Time taken to execute is : " + watch.ElapsedTicks);
+            }
+
+
+
+        }
+
+        public float HighestGrade(List<float> gs)
+        {
+            float HG = 0;
+
+            foreach (float g in gs)
+            {
+                if (g > HG)
+                {
+                    HG = g;
+                }
+            }
+
+            return HG;
+        }
+
+        public float LowestGrade(List<float> gs)
+        {
+            float LG = float.MaxValue;
+
+            foreach(float g in gs)
+            {
+                if(g < LG)
+                {
+                    LG = g;
+                }
+            }
+
+            return LG;
+        }
+
+        public float AverageGrade(List<float> gs)
+        {
+            float sum = 0;
+
+            foreach(float g in gs)
+            {
+                sum = sum + g;
+            }
+
+            return sum/gs.Count;
+        }
+
+        public GradeStatistics ComputeStatistics(List<float> gs)
+        {
+            GradeStatistics stats = new GradeStatistics();
+
+            stats.HigestGrade = 0;
+            stats.LowestGrade = float.MaxValue;
+            stats.AverageGrade = 0;
+            float sum = 0;
+            foreach(float g in gs)
+            {
+
+                if(g > stats.HigestGrade)
+                {
+                    stats.HigestGrade = g;
+                }
+
+                if(g < stats.LowestGrade)
+                {
+                    stats.LowestGrade = g;
+                }
+
+                sum = sum + g;
+            }
+
+            stats.AverageGrade = sum / gs.Count;
+            return stats;
+        } 
+    }
+
+        
+}
+ 
+
